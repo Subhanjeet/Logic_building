fd7877c5a97dce525b52a9e6fef072b05cb8c767
Subhanjeet <ssubhanjeet@gmail.com>
2026-07-30 22:14:51 +0530

    initial commit

--- JAVA/Basics Problems/EvenOdd.java
+++ JAVA/Basics Problems/EvenOdd.java
@@ -13,10 +13,11 @@ class solution{
 
 public class EvenOdd {
     public static void main(String[] args) {
+        
         Scanner sc = new Scanner(System.in);
         System.out.println("Enter a number to check Even or Odd number: ");
         int number = sc.nextInt();
-        
+
         solution ans = new solution();
         ans.EvenOdd(number);
     }
