0c3869aec46be49803c1625efd75ea2160316377
SUBHANJEET SINGH <ssubhanjeet@gmail.com>
2026-08-04 00:36:05 +0530

    Time: 23 ms (9.36%) | Memory: 47.9 MB (17.14%) - LeetSync

---
+++ 645-set-mismatch/set-mismatch.java
@@ -0,0 +1,25 @@
+import java.util.HashMap;
+
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+
+        int n = nums.length;
+        int[] result = new int[2];
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int num : nums) {
+            map.put(num, map.getOrDefault(num, 0) +1);
+        }
+
+        for (int i=1; i<=n; i++) {
+
+            if (map.getOrDefault(i, 0) == 2){
+                result[0] = i;
+            }
+            if (map.getOrDefault(i, 0) == 0){
+                result[1] = i;
+            }
+        }
+        return result;
+    }
+}
